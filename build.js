aab3e41c2806431ec3457ccd6eee42de0bd5c63a
elvardk <elvar@dk.is>
2024-03-07 15:56:12 +0000

    build script

---
+++ build.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+
+const destinationFolder = "out";
+if (fs.existsSync(destinationFolder)) {
+  fs.rmSync(destinationFolder, { recursive: true, force: true });
+}
+fs.mkdirSync(destinationFolder);
+
+const ignore = [
+  destinationFolder,
+  "build.js",
+  ".git",
+  ".gitignore",
+  "package-lock.json",
+  "doc",
+  "tests",
+];
+const filenames = fs.readdirSync(__dirname);
+const filteredFilenames = filenames.filter((filename) => {
+  return !ignore.includes(filename);
+});
+filteredFilenames.forEach((filename) => {
+  fs.cpSync(filename, `${destinationFolder}/${filename}`, {
+    recursive: true,
+    force: true,
+  });
+});
